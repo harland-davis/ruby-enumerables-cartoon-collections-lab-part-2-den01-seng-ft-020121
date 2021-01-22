0eb6bc9c377a3208876a6296bded18587552e2da
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-22 00:33:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,6 +31,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element|
-    element.include?(valid_calls) 
+    valid_calls.include(element)
     end 
 end
